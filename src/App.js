619208e5398a89c810c28edb0a51e6d264b8966a
Tamas Bordak <tamas@webtar.hu>
2020-02-27 15:17:02 +0100

    remove unused

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import Title from './components/title';
 import Chart from './components/charts';
 
 //imported the required files
-
-//API key for access to apixu API
-const key = '2b08e16a3c7349ac94360638191404';
 
 //main APP component
 class App extends React.Component {
